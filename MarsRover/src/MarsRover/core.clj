5edd53182704c447d96c1a53242794808c0d54a7
Huang Liang <exceedhl@gmail.com>
2012-02-22 18:26:31 +1100

    added marks for plateau

--- MarsRover/src/MarsRover/core.clj
+++ MarsRover/src/MarsRover/core.clj
@@ -1,6 +1,11 @@
 (ns MarsRover.core)
 
-(defrecord Rover [x y direction])
+(defrecord Plateau [x y marks])
+(defrecord Rover [x y direction dead?])
+
+(def *plateau* ^{:private true} (ref nil))
+
+(def *marks* ^{:private true} (ref #{}))
 
 (def action-matrix
   ^{:private true}
@@ -9,6 +14,9 @@
    :S {:left :E :right :W :dx 0 :dy -1}
    :E {:left :N :right :S :dx 1 :dy 0}})
 
+(defn init-plateau [x y]
+  (dosync (ref-set *plateau* (Plateau. x y []))))
+
 (defn left [rover]
   (assoc rover :direction (:left ((:direction rover) action-matrix))))
 
@@ -16,9 +24,14 @@
   (assoc rover :direction (:right ((:direction rover) action-matrix))))
 
 (defn move [rover]
-  (assoc rover
+  (if (contains? @*marks* (dissoc rover :dead?))
+    rover
+    (assoc rover
     :x (+ (:dx ((:direction rover) action-matrix)) (:x rover))
-    :y (+ (:dy ((:direction rover) action-matrix)) (:y rover))))
+    :y (+ (:dy ((:direction rover) action-matrix)) (:y rover)))))
+
+(defn- isFallenOut? [rover]
+  (or (> (:x rover) (:x @*plateau*)) (> (:y rover) (:y @*plateau*))))
 
 (defn- parse-commands [commands]
   (map #(case %
@@ -30,7 +43,10 @@
 (defn execute [rover commands]
   (loop [r rover
          cl (parse-commands commands)]
-    (if (empty? cl)
-      r
-      (recur ((first cl) r) (rest cl)))))
+    (cond (empty? cl) r
+          (isFallenOut?  ((first cl) r))
+          (do
+            (dosync (alter *marks* conj (dissoc r :dead?)))
+            (assoc r :dead? true))
+          true (recur ((first cl) r) (rest cl)))))
 
--- MarsRover/test/MarsRover/test/core.clj
+++ MarsRover/test/MarsRover/test/core.clj
@@ -1,23 +1,30 @@
 (ns MarsRover.test.core
   (:use [MarsRover.core])
-  (:use [midje.sweet]))
+  (:use [midje.sweet])
+  (:import (MarsRover.core Rover)))
 
-(let [rover (Rover. 1 2 :N)]
+(init-plateau 5 5)
+
+(let [rover (Rover. 1 2 :N false)
+      rover2 (Rover. 3 3 :E false)
+      rover3 (Rover. 4 1 :S false)]
   (facts "basic operations about rover"
          "turn left"
-         (left rover) => (Rover. 1 2 :W)
-         (left (left rover)) => (Rover. 1 2 :S)
-         (left (left (left rover))) => (Rover. 1 2 :E)
+         (left rover) => (Rover. 1 2 :W false)
+         (left (left rover)) => (Rover. 1 2 :S false)
+         (left (left (left rover))) => (Rover. 1 2 :E false)
          (left (left (left (left rover)))) => rover
          "turn right"
-         (right rover) => (Rover. 1 2 :E)
-         (right (right rover)) => (Rover. 1 2 :S)
-         (right (right (right rover))) => (Rover. 1 2 :W)
+         (right rover) => (Rover. 1 2 :E false)
+         (right (right rover)) => (Rover. 1 2 :S false)
+         (right (right (right rover))) => (Rover. 1 2 :W false)
          (right (right (right (right rover)))) => rover
          "move around"
-         (move rover) => {:x 1 :y 3 :direction :N})
+         (move rover) => (Rover. 1 3 :N false))
 
   (facts "executing commands"
-         (execute rover "LMLMLMLMM") => (Rover. 1 3 :N)
-         (execute (Rover. 3 3 :E) "MMRMMRMRRM") => (Rover. 5 1 :E)))
+         (execute rover "LMLMLMLMM") => (Rover. 1 3 :N false)
+         (execute rover2 "MMRMMRMRRM") => (Rover. 5 1 :E false)
+         (execute rover2 "MMRMMLMRRM") => (Rover. 5 1 :E true)
+         (execute rover3 "MLMLMRMRM") => (Rover. 5 0 :S false)))
 
