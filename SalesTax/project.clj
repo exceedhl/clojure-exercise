f77a1e2a730e341c01fe08879a41c61f7f9197b3
Huang Liang <exceedhl@gmail.com>
2012-02-29 22:18:07 +1100

    added SalesTax and use autotest

---
+++ SalesTax/project.clj
@@ -0,0 +1,7 @@
+(defproject SalesTax "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0-alpha4"]
+		 [com.stuartsierra/lazytest "2.0.0-SNAPSHOT"]]
+  :dev-dependencies [[com.stuartsierra/lazytest "2.0.0-SNAPSHOT"]]
+  :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"
+		 "stuartsierra-snapshots" "http://stuartsierra.com/m2snapshots"})
---
+++ SalesTax/test/SalesTax/test/core.clj
@@ -0,0 +1,7 @@
+(ns SalesTax.test.core
+  (:use [SalesTax.core])
+  (:use [lazytest.deftest]))
+
+(deftest sample
+  (is true "No tests have been written."))
+
